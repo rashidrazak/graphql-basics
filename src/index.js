ec80d38e32d4b98e8c649bd318f0f3e87e9d5436
Rashid Razak <bib.nexus@gmail.com>
2020-01-15 01:05:50 +0800

    Add schema, add resolvers, and start the GraphQL server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello GraphQL');
+import { GraphQLServer } from 'graphql-yoga'
+
+/**
+ * Type Definitions (Schema)
+ */
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`
+
+
+/**
+ * Resolvers
+ */
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query!'
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers
+})
+
+server.start(() => {
+  console.log(`The server is up!`)
+})
