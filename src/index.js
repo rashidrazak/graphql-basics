850c6fc7cfbffa95cf6ca548d861e1bf6afbc297
Rashid Razak <bib.nexus@gmail.com>
2020-01-15 23:32:55 +0800

    Array in custom types

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+// Demo user data
+const users = [
+  {
+    id: '1',
+    name: 'Rashid Razak',
+    email: 'rashid@gmail.com',
+    age: 29
+  },
+  {
+    id: '2',
+    name: 'Shairah Rahman',
+    email: 'shairah@gmail.com',
+    age: 29
+  },
+  {
+    id: '3',
+    name: 'Hamzah',
+    email: 'hamzah@gmail.com',
+    age: 4
+  },
+  {
+    id: '1',
+    name: 'Umar',
+    email: 'umar@gmail.com',
+    age: 1
+  },
+]
+
 /**
  * Type Definitions (Schema)
  */
 const typeDefs = `
   type Query {
-    greeting(name: String) : String!
-    add(numbers: [Float!]!): Float!
-    grades: [Int!]!
+    users: [User!]!
     me: User!
     post: Post!
   }
@@ -33,23 +59,8 @@ const typeDefs = `
  */
 const resolvers = {
   Query: {
-    greeting(parent, args, ctx, info) {
-      console.log(args)
-
-      if (args.name) {
-        return `Hello, ${args.name}!`
-      }
-
-      return 'Hello!'
-    },
-    add(parent, args, ctx, info) {
-      if (args.numbers.length === 0) return 0
-      return args.numbers.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue
-      })
-    },
-    grades(parent, args, ctx, info) {
-      return [96, 95, 98]
+    users(parent, args, ctx, info) {
+      return users
     },
     me() {
       return {
