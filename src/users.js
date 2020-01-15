40206a59b3641f0760122ed5ce45d5a4f6045aba
Rashid Razak <bib.nexus@gmail.com>
2020-01-15 23:51:41 +0800

    Move users demo data to a separate file

---
+++ src/users.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Demo user data
+const users = [
+  {
+    id: '1',
+    name: 'Rashid Razak',
+    email: 'rashid@gmail.com',
+    age: 29
+  },
+  {
+    id: '2',
+    name: 'Shairah Rahman',
+    email: 'shairah@gmail.com',
+    age: 29
+  },
+  {
+    id: '3',
+    name: 'Hamzah',
+    email: 'hamzah@gmail.com',
+    age: 4
+  },
+  {
+    id: '1',
+    name: 'Umar',
+    email: 'umar@gmail.com',
+    age: 1
+  },
+]
+
+export default users
