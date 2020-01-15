42996e900e4accdb0f670736d194ab7655a244fc
Rashid Razak <bib.nexus@gmail.com>
2020-01-15 23:52:41 +0800

    Add posts demo data

---
+++ src/posts.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Demo posts data
+const posts = [
+  {
+    id: '10',
+    title: 'GraphQL 101',
+    body: 'This is an introduction to GraphQL...',
+    published: true
+  },
+  {
+    id: '11',
+    title: 'Introduction to Svelte',
+    body: 'Svelte sounds sucks but...',
+    published: true
+  },
+  {
+    id: '12',
+    title: 'Flutter is Killing React Native',
+    body: 'Yeah! You heard that right. React Native is dying...',
+    published: true
+  },
+  {
+    id: '13',
+    title: 'Audio Programming Fundamentals',
+    body: 'I hate audio programming. The waveform...',
+    published: false
+  },
+]
+
+export default posts
