44d6b1d741e83c40fbfc4acadf966d1284fbbecb
Rashid Razak <bib.nexus@gmail.com>
2020-01-21 01:08:48 +0800

    Subscription: basic demo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
 import User from './resolvers/User'
 import Post from './resolvers/Post'
 import Comment from './resolvers/Comment'
@@ -14,6 +15,7 @@ const server = new GraphQLServer({
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment
