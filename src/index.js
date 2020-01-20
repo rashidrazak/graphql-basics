27d04df23eeda9da5652ee0863d56f7805f5d8d5
Rashid Razak <bib.nexus@gmail.com>
2020-01-20 22:49:56 +0800

    Refactor directory structure: Separate schema file

--- src/index.js
+++ src/index.js
@@ -5,73 +5,6 @@ import users from './users'
 import posts, { setPosts } from './posts'
 import comments, { setComments } from './comments'
 
-/**
- * Type Definitions (Schema)
- */
-const typeDefs = `
-  type Query {
-    users(query: String): [User!]!
-    posts(query: String): [Post!]!
-    me: User!
-    post: Post!
-    comment: [Comment!]!
-  }
-
-  type Mutation {
-    createUser(data: CreateUserInput): User!
-    deleteUser(id: ID!): User!
-    createPost(data: CreatePostInput): Post!
-    deletePost(id: ID!): Post!
-    createComment(data: CreateCommentInput): Comment!
-    deleteComment(id: ID!): Comment!
-  }
-
-  input CreateUserInput {
-    name: String!
-    email: String!
-    age: Int
-  }
-
-  input CreatePostInput {
-    title: String!
-    body: String!
-    published: Boolean!
-    author: ID!
-  }
-
-  input CreateCommentInput {
-    text: String!
-    author: ID!
-    published: Boolean!
-    post: ID!
-  }
-
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-    posts: [Post!]!
-    comments: [Comment!]!
-  }
-
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-    author: User!
-    comments: [Comment!]!
-  }
-
-  type Comment {
-    id: ID!
-    text: String!
-    author: User!
-    post: Post!
-  }
-`
-
 
 /**
  * Resolvers
@@ -226,7 +159,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({
-  typeDefs: typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers: resolvers
 })
 
